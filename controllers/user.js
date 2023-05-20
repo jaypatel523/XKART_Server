389f5c9f53aa52ddc3f06acf23f3bf0c5ca08d69
Jay <jaypatel0523@gmail.com>
2023-05-20 15:45:27 +0530

    changes done

--- controllers/user.js
+++ controllers/user.js
@@ -297,29 +297,13 @@ const getUser = async (req, res) => {
 }
 
 
-const requireSignin = jwt({
-  secret: process.env.JWT_SECRET,
-  userProperty: "auth",
-  algorithms: ["HS256"],
-});
-
-const hasAuthorization = (req, res, next) => {
-  const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
-  if (!authorized) {
-    return res.status(403).json({ error: "User is not authorized" });
-  }
-  next();
-};
-
 module.exports = {
   login,
   logout,
   register,
-  hasAuthorization,
   getUserDetails,
   adminLogin,
   loginWithGoogle,
-  requireSignin,
   registerWithGoogle,
   generateOTP,
   getUser
